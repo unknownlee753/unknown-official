05486a5fad3373a5b5c481c359ef7e60cc7956a5
unknownlee753 <90024284+unknownlee753@users.noreply.github.com>
2021-09-11 16:26:30 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -82,7 +82,7 @@ class Progress:
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress +"│" + "\n**├💫Total Size :-**   〘<code>{1}</code>〙\n**├💾Uploaded :-** 〘<code>{0}</code>〙\n**├🚀Speed** :-  〘<code>{2}/s</code>〙\n**├⏳ETA** :-  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ @HiroshiBots ⌉**".format(
+            tmp = progress +"│" + "\n**├💫Total Size :-**   〘<code>{1}</code>〙\n**├💾Uploaded :-** 〘<code>{0}</code>〙\n**├🚀Speed** :-  〘<code>{2}/s</code>〙\n**├⏳ETA** :-  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ @unknownleech ⌉**".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
